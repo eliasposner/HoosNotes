2058e4341fcec410d235803b9226d6c80cf0acd0
Keith <kab7em@virginia.edu>
2021-10-15 23:13:05 -0400

    Replace dummy test case with login attempt tests

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,8 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 import unittest
 
-
 # Create your tests here.
-class LoginTestCase(unittest.TestCase):
-	def test_dummy_test_case(self):
-		self.assertEqual(2, 2)
+
+class LoginTest(TestCase):
+	def setUp(self):
+		User = get_user_model()
+		user = User.objects.create_user(username='test', password='t3st!!', email='test@email.com')
+		user.save()
+
+	# Test case adapted from Professor Sherrif's lecture on 10/7/2021 at 11am
+	def test_correct_credentials(self):
+		c = Client()
+		logged_in = c.login(username='test', password='t3st!!', email='test@email.com')
+		self.assertTrue(logged_in)
+
+	def test_incorrect_password(self):
+		c = Client()
+		logged_in = c.login(username='test', password='T3st!!', email='test@email.com')
+		self.assertFalse(logged_in)
+
+

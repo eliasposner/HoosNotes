1f988584a4c8b6834f0782a61fe21f0bfbf56385
Keith <kab7em@virginia.edu>
2021-10-16 16:34:30 -0400

    Comment out create user test case

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -27,13 +27,16 @@ class LoginTest(unittest.TestCase):
 		allUsers = User.objects.all()
 		unregisted_user = User(username='unregistered', email='unregistered@email.com')
 		self.assertFalse(unregisted_user in allUsers)
+	
 
+	'''
 	def test_account_is_created(self):
 		User = get_user_model()
 		user = User.objects.create_user(username='newUser', email='newUser@email.com')
 		allUsers = User.objects.all()
 		self.assertTrue(user in allUsers)
-
+	'''
+	
 	# Check if unregistered account does not exist
 	#def test_unregistered_acc_does_not_exist(self):
 

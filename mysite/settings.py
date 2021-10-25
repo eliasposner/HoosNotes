507aaa8b3a0532e56c53da7cdc9467f5d7874886
eliasposner <eliasposner@gmail.com>
2021-10-24 23:40:11 -0400

    Added header and footer to home page

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
+    'bootstrap5',
     'scheduler.apps.SchedulerConfig',
     'django.contrib.admin',
     'django.contrib.sites',

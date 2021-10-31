60c58f357342e0c9fd00bcfe0b7c499b70320400
keithb32 <77218986+keithb32@users.noreply.github.com>
2021-10-31 00:17:54 -0400

    Add fields to StudentClass, style ClassListView

--- scheduler/models.py
+++ scheduler/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import datetime
+
 # Create your models here.
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "profile", null=True)
@@ -23,13 +25,17 @@ def save_user_profile(sender, instance, **kwargs):
 	instance.profile.save()
 
 class StudentClass(models.Model):
-	class_name = models.CharField(max_length=200, default ='')
+	class_name = models.CharField(max_length=200, default='')
 	instructor = models.CharField(max_length=200, default='')
 	users = models.ManyToManyField(Profile)
+	start_time = models.TimeField(null=True, help_text='Please use the following format:  HH:MM')
+	end_time = models.TimeField(null=True, help_text='Please use the following format:  HH:MM')
+	location = models.CharField(max_length=200, default='')
 	enrolled_users_count = models.IntegerField(default=0)
 
 	class Meta:
 		verbose_name_plural = 'Student Classes'
+		unique_together = [['class_name', 'instructor']]
 
 	def __str__(self):
 		return self.class_name

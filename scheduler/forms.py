a799803500aafbbf1adea10b3267bcdbcf200340
keithb32 <77218986+keithb32@users.noreply.github.com>
2021-12-03 19:22:00 -0500

    Remove end_time field from Event, miscellaneous style changes

--- scheduler/forms.py
+++ scheduler/forms.py
@@ -13,15 +13,13 @@ class EventForm(ModelForm):
     # datetime-local is a HTML5 input type, format to make date time show on fields
     widgets = {
       'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
-      'end_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
     }
-    fields = ('title', 'description', 'start_time', 'end_time')
+    fields = ('title', 'description', 'start_time')
 
   def __init__(self, *args, **kwargs):
     super(EventForm, self).__init__(*args, **kwargs)
     # input_formats to parse HTML5 datetime-local input to datetime field
     self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
-    self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)
 
 class JoinForm(forms.Form):
     classes = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
